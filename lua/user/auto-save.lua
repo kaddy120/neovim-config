c382f29d42e200fd562084f06313c29b70233361
kaddy120 <kaddy120@gmail.com>
2023-09-22 03:25:24 +0200

    fix an error preventing :GBrowser to open a browser

--- lua/user/auto-save.lua
+++
@@ -1,7 +0,0 @@
-local ok_status, auto_save = pcall(require, 'auto-save')
-if not ok_status then
-  return
-end
-
---- empty setup using defaults
-auto_save.setup()
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -73,3 +73,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- enable :GBrowser to open a browser
+vim.cmd("command! -nargs=1 Browse silent execute '!open' shellescape(<q-args>,1)")
